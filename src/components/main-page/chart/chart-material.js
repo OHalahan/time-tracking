54812c6c8c4f223340f7431f2a556b525600cf64
Oleksandr Halahan <Oleksandr.Halahan@portaone.com>
2017-10-30 17:20:43 +0200

    Implemented Info page

--- src/components/main-page/chart/chart-material.js
+++ src/components/main-page/chart/chart-material.js
@@ -2,11 +2,11 @@ const primaryColor = '#64B5F6';
 
 const styles = {
     width: '100%',
-    margin: { 
-        top: 5, 
-        right: 30, 
-        left: 20, 
-        bottom: 5 
+    margin: {
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
     },
     color: primaryColor
 }
